d75390b98eeb8c71d1b78f66d3c53346a23c167c
santosh <santosh.ece06@gmail.com>
2021-05-29 10:58:41 +0530

    modules basics.

---
+++ calculator.js
@@ -0,0 +1,8 @@
+let title = "Project Title";
+
+let sum = (n1, n2) => n1 + n2;
+
+let diff = (n1, n2) => n2 - n1;
+
+// public
+export { sum, diff };
---
+++ main.js
@@ -0,0 +1,6 @@
+// local module must starts with ./
+import { sum } from "./calculator.js";
+
+console.log("hello worlddd");
+let total = sum(10, 20);
+console.log(total);
