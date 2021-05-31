087b7775cd5b1738ab961a09888f099a5725db49
santosh <santosh.ece06@gmail.com>
2021-05-31 09:59:55 +0530

    working with modules.

---
+++ src/my-module5.js
@@ -0,0 +1,19 @@
+let username = "rohit";
+let total = 1000;
+let refObj = {};
+let list = [];
+
+let sayHello = () => "Hello World!!";
+let addition = (n1, n2) => n1 + n2; // Lamda/Arrow Function
+
+function additionV1(n1, n2) {
+  return n1 + n2;
+}
+
+export { username, total, refObj };
+export { list, addition, additionV1 };
+
+// special member and ONLY one member can be default member
+// 1. there is no curly braces
+// 2. there is default keyword
+export default sayHello;
