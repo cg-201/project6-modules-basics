de5e855e3b526291f61cdb0b3c59409252a86a1c
santosh <santosh.ece06@gmail.com>
2021-05-31 10:00:48 +0530

    moduels. basics.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { username } from "./my-module3.js";
+import { username, addition } from "./my-module3.js";
+import { list } from "./my-module3";
 import sayHello from "./my-module3.js";
 
 console.log(username);
