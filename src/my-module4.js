5d60231ab084cb70c0a146aff41b8e62569a1c71
santosh <santosh.ece06@gmail.com>
2021-05-31 09:59:09 +0530

    wokring with moudles.

---
+++ src/my-module4.js
@@ -0,0 +1,11 @@
+export let username = "rohit";
+export let total = 1000;
+export let refObj = {};
+export let list = [];
+
+export default sayHello = () => "Hello World!!";
+export let addition = (n1, n2) => n1 + n2; // Lamda/Arrow Function
+
+export function additionV1(n1, n2) {
+  return n1 + n2;
+}
